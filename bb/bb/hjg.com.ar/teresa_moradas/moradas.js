7af45b0b7da5080c5cc96d857dd73eb974384a1a
dbaezh78 <dbaezh78@gmail.com>
2024-05-30 22:19:20 -0400

    update

--- bb/bb/hjg.com.ar/teresa_moradas/moradas.js
+++
@@ -1,6 +0,0 @@
-function nota(url) {
-	popup = window.open(url,'notas','status=no,toolbar=yes,scrollbars=yes,resizable=yes,width=600,height=70,top=0,left=0');
-	if (popup.focus) popup.focus();
-	return false;
-}				
-
