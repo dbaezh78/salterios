335b81e509e306914f48f833aa91cf990be86836
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2024-07-15 12:50:07 -0400

    update

---
+++ update/catalogo.js
@@ -0,0 +1,29 @@
+const catalogo = {
+					// Inicio Santo
+	"11/7/2024": {
+        salmodia: "/salterios/tiempo/santos/julio/03-SANTO TOMÁS, apóstol",
+        santo: {
+            nombre: "SANTO TOMÁS, apóstol",
+            enlace: "/salterios/tiempo/santos/julio/03-SANTO TOMÁS, apóstol"
+        }
+    },
+				//Fin Santo
+	
+    "12/7/2024": {
+        salmodia: "/salterios/tiempo/santos/julio/09-NUESTRA SEÑORA DE ITATÍ",
+        santo: {
+            nombre: "09-NUESTRA SEÑORA DE ITATÍ",
+            enlace: "/salterios/tiempo/santos/julio/09-NUESTRA SEÑORA DE ITATÍ"
+        }
+    },
+				// Inicio Santo
+	"13/7/2024": {
+        salmodia: "enlace_a_salmodia_12_julio.html",
+        santo: {
+            nombre: "San Benito",
+            enlace: "enlace_a_san_benito.html"
+        }
+    },
+				//Fin Santo
+};
+
---
+++ update/catalogosanto.js
@@ -0,0 +1,58 @@
+const catalogoSanto = {
+
+	//Inicio de Registro de los Santos del Mes de Enero
+		"1/1": { nombre: "Santa_María_Madre_De_Dios", enlace: "/salterios/tiempo/santos/enero/01-SANTA_MARÍA_MADRE_DE_DIOS" },
+		"2/1": { nombre: "Santos_Basilio_Magno_Y_Gregorio_Nacianzo", enlace: "/salterios/tiempo/santos/enero/02-SANTOS_BASILIO_MAGNO_Y_GREGORIO_NACIANZO" },
+		"17/1": { nombre: "San Antonio, Abad", enlace: "/salterios/tiempo/santos/enero/17-SAN ANTONIO, ABAD" },
+		"21/1": { nombre: "Santa Inés, Virgen Y Mártir", enlace: "/salterios/tiempo/santos/enero/21-SANTA INÉS, virgen y mártir" },
+		"24/1": { nombre: "Santa María, Reina De La Paz-San Francisco De Sales-San Francisco De Sales", enlace: "/salterios/tiempo/santos/enero/24-SANTA MARÍA, REINA DE LA PAZ-SAN FRANCISCO DE SALES-SAN FRANCISCO DE SALES" },
+		"25/1": { nombre: "La Conversión De San Pablo", enlace: "/salterios/tiempo/santos/enero/25-LA CONVERSIÓN DE SAN PABLO" },
+		"26/1": { nombre: "Santos Timoteo Y Tito, Obispos", enlace: "/salterios/tiempo/santos/enero/26-SANTOS TIMOTEO Y TITO, obispos" },
+		"28/1": { nombre: "Santo Tomás De Aquino", enlace: "/salterios/tiempo/santos/enero/28-SANTO TOMÁS DE AQUINO" },
+		"31/1": { nombre: "San Juan Bosco", enlace: "/salterios/tiempo/santos/enero/31-SAN JUAN BOSCO" },
+	//Fin de Registro de los Santos del Mes de Enero
+
+	//Inicio de Registro de los Santos del Mes de Febrero
+		"1/2": { nombre: "La Presentación Del Señor", enlace: "/salterios/tiempo/santos/febrero/02-LA PRESENTACIÓN DEL SEÑOR" },
+		"2/2": { nombre: "Santa Águeda, Virgen Y Mártir", enlace: "/salterios/tiempo/santos/febrero/04-SANTA ÁGUEDA, virgen y mártir" },
+		"17/2": { nombre: "San Felipe De Jesús", enlace: "/salterios/tiempo/santos/febrero/05-SAN FELIPE DE JESÚS" },
+		"21/2": { nombre: "Santos Pablo Miki Y Compañeros", enlace: "/salterios/tiempo/santos/febrero/06-SANTOS PABLO MIKI Y COMPAÑEROS" },
+		"24/2": { nombre: "Santa Escolástica, Virgen", enlace: "/salterios/tiempo/santos/febrero/10-SANTA ESCOLÁSTICA, virgen" },
+		"25/2": { nombre: "Santos Cirilo, Monje Y Metodio, Obispo", enlace: "/salterios/tiempo/santos/febrero/14-SANTOS CIRILO, monje y METODIO, obispo" },
+		"26/2": { nombre: "La Cátedra De San Pedro, Apóstol", enlace: "/salterios/tiempo/santos/febrero/22-LA CÁTEDRA DE SAN PEDRO, APÓSTOL" },
+		"28/2": { nombre: "San Policarpo, Obispo Y Mártir", enlace: "/salterios/tiempo/santos/febrero/23-SAN POLICARPO, obispo y mártir" },
+	//Fin de Registro de los Santos del Mes de Febrero
+
+	//Inicio de Registro de los Santos del Mes de Marzo
+		"1/2": { nombre: "La Presentación Del Señor", enlace: "/salterios/tiempo/santos/febrero/02-LA PRESENTACIÓN DEL SEÑOR" },
+		"2/2": { nombre: "Santa Águeda, Virgen Y Mártir", enlace: "/salterios/tiempo/santos/febrero/04-SANTA ÁGUEDA, virgen y mártir" },
+		"17/2": { nombre: "San Felipe De Jesús", enlace: "/salterios/tiempo/santos/febrero/05-SAN FELIPE DE JESÚS" },
+		"21/2": { nombre: "Santos Pablo Miki Y Compañeros", enlace: "/salterios/tiempo/santos/febrero/06-SANTOS PABLO MIKI Y COMPAÑEROS" },
+		"24/2": { nombre: "Santa Escolástica, Virgen", enlace: "/salterios/tiempo/santos/febrero/10-SANTA ESCOLÁSTICA, virgen" },
+		"25/2": { nombre: "Santos Cirilo, Monje Y Metodio, Obispo", enlace: "/salterios/tiempo/santos/febrero/14-SANTOS CIRILO, monje y METODIO, obispo" },
+		"26/2": { nombre: "La Cátedra De San Pedro, Apóstol", enlace: "/salterios/tiempo/santos/febrero/22-LA CÁTEDRA DE SAN PEDRO, APÓSTOL" },
+		"28/2": { nombre: "San Policarpo, Obispo Y Mártir", enlace: "/salterios/tiempo/santos/febrero/23-SAN POLICARPO, obispo y mártir" },
+	//Fin de Registro de los Santos del Mes de Marzo
+
+
+	//Inicio de Registro de los Santos del Mes de Julio
+	"3/7": { nombre: "Santo Tomás, Apóstol", enlace: "/salterios/tiempo/santos/julio/03-SANTO TOMÁS, apóstol" },
+	 "9/7": [
+	{nombre: "Nuestra Señora De Itatí", enlace: "/salterios/tiempo/santos/julio/09-NUESTRA SEÑORA DE ITATÍ" },
+	{nombre: "Nuestra Señora Del Rosario De Chiquinquirá, Patrona De Colombia",enlace: "/salterios/tiempo/santos/julio/09-NUESTRA SEÑORA DEL ROSARIO DE CHIQUINQUIRÁ, patrona de Colombia"},    ],
+	"11/7": { nombre: "San Benito Abad, Patrono De Europa", enlace: "/salterios/tiempo/santos/julio/11-SAN BENITO ABAD, patrono de Europa" },
+	"12/7": { nombre: "San Benito Abad, Patrono De Europa", enlace: "/salterios/tiempo/santos/julio/11-SAN BENITO ABAD, patrono de Europa" },
+	"15/7": { nombre: "San Buenaventura, Obispo Y Doctor De La Iglesia", enlace: "/salterios/tiempo/santos/julio/15-SAN BUENAVENTURA, obispo y doctor de la Iglesia" },
+	"16/7": { nombre: "Nuestra Señora Del Carmen", enlace: "/salterios/tiempo/santos/julio/16-NUESTRA SEÑORA DEL CARMEN" },
+	"22/7": { nombre: "Santa María Magdalena", enlace: "/salterios/tiempo/santos/julio/22-SANTA MARÍA MAGDALENA" },
+	"24/7": { nombre: "San Francisco Solano, Presbítero", enlace: "/salterios/tiempo/santos/julio/24-SAN FRANCISCO SOLANO, presbítero" },
+	"25/7": { nombre: "Santiago, Apóstol", enlace: "/salterios/tiempo/santos/julio/25-SANTIAGO, apóstol" },
+	"26/7": { nombre: "Santos Joaquín Y Ana, Padres De La Santísima Virgen María", enlace: "/salterios/tiempo/santos/julio/26-SANTOS JOAQUÍN Y ANA, padres de la Santísima Virgen María" },
+	"29/7": { nombre: "Santa Marta", enlace: "/salterios/tiempo/santos/julio/29-SANTA MARTA" },
+	"31/7": { nombre: "San Ignacio De Loyola, Presbítero", enlace: "/salterios/tiempo/santos/julio/31-SAN IGNACIO DE LOYOLA, presbítero" },
+	//Fin de Registro de los Santos del Mes de Julio
+
+
+}; 
+
+
