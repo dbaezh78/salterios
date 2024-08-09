8197354cbd38e669cb2e48e9ed5df88161666ef8
dbaezh78 <dbaezh78@gmail.com>
2024-08-09 16:57:50 -0400

    update

--- update/catalogosanto.js
+++ update/catalogosanto.js
@@ -1,5 +1,5 @@
 const catalogoSanto = {
-
+	// **************************************************** ENERO ****************************************************
 	//Inicio de Registro de los Santos del Mes de Enero
 		"1/1": { nombre: "Santa_María_Madre_De_Dios", enlace: "/salterios/tiempo/santos/enero/01-SANTA_MARÍA_MADRE_DE_DIOS" },
 		"2/1": { nombre: "Santos_Basilio_Magno_Y_Gregorio_Nacianzo", enlace: "/salterios/tiempo/santos/enero/02-SANTOS_BASILIO_MAGNO_Y_GREGORIO_NACIANZO" },
@@ -12,6 +12,8 @@ const catalogoSanto = {
 		"31/1": { nombre: "San Juan Bosco", enlace: "/salterios/tiempo/santos/enero/31-SAN JUAN BOSCO" },
 	//Fin de Registro de los Santos del Mes de Enero
 
+
+	// **************************************************** FEBRERO ****************************************************
 	//Inicio de Registro de los Santos del Mes de Febrero
 		"1/2": { nombre: "La Presentación Del Señor", enlace: "/salterios/tiempo/santos/febrero/02-LA PRESENTACIÓN DEL SEÑOR" },
 		"2/2": { nombre: "Santa Águeda, Virgen Y Mártir", enlace: "/salterios/tiempo/santos/febrero/04-SANTA ÁGUEDA, virgen y mártir" },
@@ -23,6 +25,7 @@ const catalogoSanto = {
 		"28/2": { nombre: "San Policarpo, Obispo Y Mártir", enlace: "/salterios/tiempo/santos/febrero/23-SAN POLICARPO, obispo y mártir" },
 	//Fin de Registro de los Santos del Mes de Febrero
 
+	// **************************************************** MARZO ****************************************************
 	//Inicio de Registro de los Santos del Mes de Marzo
 		"1/2": { nombre: "La Presentación Del Señor", enlace: "/salterios/tiempo/santos/febrero/02-LA PRESENTACIÓN DEL SEÑOR" },
 		"2/2": { nombre: "Santa Águeda, Virgen Y Mártir", enlace: "/salterios/tiempo/santos/febrero/04-SANTA ÁGUEDA, virgen y mártir" },
@@ -34,8 +37,15 @@ const catalogoSanto = {
 		"28/2": { nombre: "San Policarpo, Obispo Y Mártir", enlace: "/salterios/tiempo/santos/febrero/23-SAN POLICARPO, obispo y mártir" },
 	//Fin de Registro de los Santos del Mes de Marzo
 
-
+	// **************************************************** ABRIL ****************************************************
+	
+	// **************************************************** MAYO *****************************************************
+	
+	// **************************************************** JUNIO *****************************************************
+	
+	// **************************************************** JULIO ****************************************************
 	//Inicio de Registro de los Santos del Mes de Julio
+	// Si te fijas el día 9 de Julio que es 9/7 tiene dos Santos, asi tienes que hacerlo para cuando pase la misma insidencia.
 	"3/7": { nombre: "Santo Tomás, Apóstol", enlace: "/salterios/tiempo/santos/julio/03-SANTO TOMÁS, apóstol" },
 	 "9/7": [
 	{nombre: "Nuestra Señora De Itatí", enlace: "/salterios/tiempo/santos/julio/09-NUESTRA SEÑORA DE ITATÍ" },
@@ -51,6 +61,40 @@ const catalogoSanto = {
 	"29/7": { nombre: "Santa Marta", enlace: "/salterios/tiempo/santos/julio/29-SANTA MARTA" },
 	"31/7": { nombre: "San Ignacio De Loyola, Presbítero", enlace: "/salterios/tiempo/santos/julio/31-SAN IGNACIO DE LOYOLA, presbítero" },
 	//Fin de Registro de los Santos del Mes de Julio
+
+	// **************************************************** AGOSTO *****************************************************
+	
+	"1/8": { nombre: "San Alfonso María De Ligorio", enlace: "/salterios/tiempo/santos/agosto/01-SAN ALFONSO MARÍA DE LIGORIO" },
+	"4/8": { nombre: "San Juan María Vianney, Presbítero", enlace: "/salterios/tiempo/santos/agosto/04-SAN JUAN MARÍA VIANNEY, presbítero" },
+	"6/8": { nombre: "La Transfiguración Del Señor", enlace: "/salterios/tiempo/santos/agosto/06-LA TRANSFIGURACIÓN DEL SEÑOR" },
+	"8/8": { nombre: "Santo Domingo, Presbítero", enlace: "/salterios/tiempo/santos/agosto/08-SANTO DOMINGO, presbítero" },
+	"10/8": { nombre: "San Lorenzo, Diácono Y Mártir", enlace: "/salterios/tiempo/santos/agosto/10-SAN LORENZO, diácono y mártir" },
+	"11/8": { nombre: "Santa Clara, Vírgen", enlace: "/salterios/tiempo/santos/agosto/11-SANTA CLARA, vírgen" },
+	"14/8": { nombre: "San Maximiliano María Kolbe, Presbítero Y Mártir", enlace: "/salterios/tiempo/santos/agosto/14-SAN MAXIMILIANO MARÍA KOLBE, presbítero y mártir" },
+	"15/8": { nombre: "La Asunción De La Santísima Virgen María", enlace: "/salterios/tiempo/santos/agosto/15-LA ASUNCIÓN DE LA SANTÍSIMA VIRGEN MARÍA" },
+	"20/8": { nombre: "San Bernardo, Abad Y Doctor De La Iglesia", enlace: "/salterios/tiempo/santos/agosto/20-SAN BERNARDO, abad y doctor de la Iglesia" },
+	"21/8": { nombre: "San Pio X, Papa", enlace: "/salterios/tiempo/santos/agosto/21-SAN PIO X, papa" },
+	"22/8": { nombre: "La Santísima Virgen María, Reina", enlace: "/salterios/tiempo/santos/agosto/22-LA SANTÍSIMA VIRGEN MARÍA, REINA" },
+	"23/8": { nombre: "Santa Rosa De Lima, Virgen, Patrona De América Latina", enlace: "/salterios/tiempo/santos/agosto/23-SANTA ROSA DE LIMA, virgen, Patrona de América Latina" },
+	"24/8": { nombre: "San Bartolomé, Apóstol", enlace: "/salterios/tiempo/santos/agosto/24-SAN BARTOLOMÉ, apóstol" },
+	"27/8": { nombre: "Santa Mónica", enlace: "/salterios/tiempo/santos/agosto/27-SANTA MÓNICA" },
+	"28/8": { nombre: "San Agustín, Obispo Y Doctor De La Iglesia", enlace: "/salterios/tiempo/santos/agosto/28-SAN AGUSTÍN, obispo y doctor de la iglesia" },
+	"29/8": { nombre: "El Martirio De San Juan Bautista", enlace: "/salterios/tiempo/santos/agosto/29-EL MARTIRIO DE SAN JUAN BAUTISTA" },
+	"30/8": { nombre: "Santa Rosa De Lima, Virgen, Patrona De América Latina", enlace: "/salterios/tiempo/santos/agosto/30-SANTA ROSA DE LIMA, virgen, Patrona de América Latina" },
+	//Fin de Registro de los Santos del Mes de Agosto
+
+
+	// **************************************************** SEPTIEMBRE *****************************************************
+
+
+	// **************************************************** OCTUBRE *****************************************************
+
+
+	// **************************************************** NOVIEMBRE *****************************************************
+
+
+	// **************************************************** DICIEMBRE *****************************************************
+
 
 
 }; 
